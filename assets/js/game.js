dcd030f2f69b244fbc456036f269b975b8b53bcd
Gaara <chrisimata21@gmail.com>
2022-07-19 20:17:30 -0700

    added arrays and functionality

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,12 +6,19 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Samuel", "Oscar", "Araceli"];
+
+for(var i = 0; i < enemyNames.length; i++) {
+  console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemyNames[i] + " is at " + i + " index");
+}
+
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 // fight function
-var fight = function() {
+var fight = function(enemyName) {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
@@ -67,4 +74,6 @@ var fight = function() {
 }; // end of fight function
 
 // run fight function to start game
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
